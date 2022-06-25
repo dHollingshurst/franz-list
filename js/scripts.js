810fa530ca77242f404d0526291eeb9af4edffd4
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-06-25 19:44:28 -0400

    Update scripts.js
    - this commit removes all code from e1 and replaces it with the beginnings of our football player list.  'Josh Allen' written to test retrieval of object within array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world!');
-let favFood = 'tacos';
-console.log(favFood);
-document.write(favFood);
+let playerList = [
+    { name: 'Patrick Mahomes', salary: 9500, position: ['Offense', 'QB', 'SFLX']},
+    { name: 'Josh Allen', salary: 9800, position: ['Offense', 'QB', 'SFLX']},
+    { name: 'Jonathan Taylor', salary: 10000, position: ['Offense', 'RB', 'FLX', 'SFLX']},
+    { name: 'New Orleans Saints', salary: 3500, position: ['Defense', 'DST']}
+];
+document.write(playerList[1].name);
