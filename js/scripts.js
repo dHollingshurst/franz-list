37565fc25db8efbaab3a7e71d7a83b312c8dec60
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-06-26 18:41:41 -0400

    Update scripts.js
    - this commit creates a loop which displays players and remarks on their cost if it is sufficiently high

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,21 @@ let playerList = [
     { name: 'Jonathan Taylor', salary: 10000, position: ['Offense', 'RB', 'FLX', 'SFLX']},
     { name: 'New Orleans Saints', salary: 3500, position: ['Defense', 'DST']}
 ];
-document.write(playerList[1].name);
+/*document.write(playerList[1].name);
+if (playerList) {
+    console.log('yeah');
+}
+if (playerList[1].salary < playerList[2].salary) {
+    document.write(`${playerList[0].name} is less expensive than ${playerList[1].name}`);
+}*/
+let text = "";
+let newLine = '/ln';
+for (i = 0; i < playerList.length; i++ ) {
+    if (playerList[i].salary > 9000) {
+        document.write(`<br> ${playerList[i].name} ${text} (cost: ${playerList[i].salary}) - Whoa, that ${playerList[i].name} is expensive <br>`);
+    }
+
+    else {
+        document.write(`<br> ${playerList[i].name} ${text} (cost: ${playerList[i].salary}) <br>`);
+    }
+}
