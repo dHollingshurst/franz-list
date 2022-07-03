a03d391a4a9197e7c420dbcd7a29626b59ee6451
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-07-02 20:16:30 -0400

    Update scripts.js
    - this commit puts the playerList into IIFE form, insulating it from unwanted influence. The forEach loop was modified to account for this change.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
-let playerList = [
+let playerListRepository = (function () {
+  let playerList = [
     { name: 'Patrick Mahomes', salary: 9500, position: ['Offense', 'QB', 'SFLX']},
     { name: 'Josh Allen', salary: 9800, position: ['Offense', 'QB', 'SFLX']},
     { name: 'Jonathan Taylor', salary: 10000, position: ['Offense', 'RB', 'FLX', 'SFLX']},
     { name: 'New Orleans Saints', salary: 3500, position: ['Defense', 'DST']}
 ];
+  
+  function add(player) {
+        playerList.push(player);
+    }
+  function getAll() {
+        return playerList;
+    }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 let text = "";
 // the commented code below will remain for now as an example of another way to achieve the same end as the forEach()
@@ -19,15 +32,19 @@ let text = "";
   }
 } */
 
-function printPlayerList(list) {
+playerListRepository.add({name: 'Justin Jefferson', salary: 8800, position: ['Offense', 'WR', 'FLX', 'SFLX' ]});
+playerListRepository.getAll().forEach(function(list) {
     if (list.salary > 9000) {
         document.write(`<br> ${list.name} ${text} (cost: ${list.salary}) - Whoa, that ${list.name} is expensive <br>`);
     }
     else {
         document.write(`<br> ${list.name} ${text} (cost: ${list.salary}) <br>`);
     }
-}
-playerList.forEach(printPlayerList);
+});
+
+
+
 //printPlayerList(playerList);
 
 
+console.log(playerListRepository.getAll());
