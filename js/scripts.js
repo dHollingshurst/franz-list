74f72436a2e8e8fc12a05fa89c36c3e1192b089a
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-07-10 17:06:21 -0400

    Update scripts.js
    - this commit swaps players for poke; introduces the showDetails() and loadList()

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-/*IIFE employed. protects variables. to access pokemonList[], add(), getAll(), or addListItem(), pokemonListRepository
+/*IIFE employed. protects variables. to access pokemonList[], add(), getAll(), or addListItem(), pokemonRepository
 must first be envoked - see the add() and getAll().forEach() calls below  . see js functions pt 2 exercise 1.5 of full stack emersion.*/
-let pokemonListRepository = (function () {
-  let pokemonList = [
-    { name: 'Patrick Mahomes', salary: 9500, position: ['Offense', 'QB', 'SFLX']},
-    { name: 'Josh Allen', salary: 9800, position: ['Offense', 'QB', 'SFLX']},
-    { name: 'Jonathan Taylor', salary: 10000, position: ['Offense', 'RB', 'FLX', 'SFLX']},
-    { name: 'New Orleans Saints', salary: 3500, position: ['Defense', 'DST']}
-   ];
-  
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
   function add(pokemon) {
     //adds a new pokemon to the list when executed. must pass name, salary, and positions as arguments
         pokemonList.push(pokemon);
@@ -39,26 +35,62 @@ let pokemonListRepository = (function () {
         listElement.appendChild(listItem);
     }
 
-    function showDetails(pokemon) {
+  function showDetails(pokemon) {
         // this is the function called by the button click event
+        loadDetails(pokemon).then( function() {
         console.log(pokemon);
+        });
     }
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response)
+     {
+        return response.json();
+     }).then(function (json) {
+        json.results.forEach(function (item) {
+            let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            add(pokemon);
+        });
+     }).catch(function (e) {
+        console.error(e);
+    })
+   }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (details) {
+        // now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+    }).catch(function (e) {
+        console.error(e);
+    });
+  }
+
    return {
     /* the return portion of the IIFE is essential as it is what allows you to access the listed functions elsewhere in the code.  
     the below code takes this form becuase the key and value are the same in each. could be written add: add etc. */
      add,
      getAll,
-     addListItem
+     addListItem,
+     loadList,
+     //loadDetails
   };
 })();
 
 // adding pokemon to test add(pokemon) - success
-pokemonListRepository.add({name: 'Justin Jefferson', salary: 8800, position: ['Offense', 'WR', 'FLX', 'SFLX' ]});
+// pokemonRepository.add({});
 
-pokemonListRepository.getAll().forEach(function(pokemon) {
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon) {
     /*without this call below, list does not display. this forEach() is what runs through all the array objects and executes all of 
     the addListItem() on each object of the array - gives them a button, adds event, etc */
-    pokemonListRepository.addListItem(pokemon);
+    pokemonRepository.addListItem(pokemon);
+ });
 });
-
-//console.log(pokemonListRepository.getAll());
