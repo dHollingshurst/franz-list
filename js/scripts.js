7b519e5785a2d71da6e3007173f052563d2fab3b
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-07-10 09:28:06 -0400

    Update scripts.js
    - this commit adds multiple comments and clarifies intent

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//IIFE employed. protects variables
 let playerListRepository = (function () {
   let playerList = [
     { name: 'Patrick Mahomes', salary: 9500, position: ['Offense', 'QB', 'SFLX']},
@@ -7,39 +8,50 @@ let playerListRepository = (function () {
 ];
   
   function add(player) {
+    //adds a new player to the list when executed. must pass name, salary, and positions as arguments
         playerList.push(player);
     }
   function getAll() {
+    // returns the list of players and their details
         return playerList;
     }
     function addListItem(player) {
+        // .player-list is the class of the ul
         let listElement = document.querySelector('.player-list');
         //console.log(listElement);
+        // creates li for the ul
         let listItem = document.createElement('li');
+        // creates a button and variable called button, and adds an event handler for the click event. handler prints name to log
         let button = document.createElement('button');
         button.addEventListener('click', function() {
             showDetails(player);
         });
         button.innerText = player.name;
         console.log(button.innerText);
+        //gives variable button a class so it can be uniquely manipulated in css
         button.classList.add('nameButton');
+        // attaches button to each li
         listItem.appendChild(button);
+        // attachs the li to the ul. created elements need to be appended. 
         listElement.appendChild(listItem);
     }
     function showDetails(player) {
+        // this is the function called by the button click event on lines 25-26
         console.log(player);
     }
-  return {
-    // the below code takes this form becuase the key and value are the same in each. add: add etc.
+   return {
+    // the below code takes this form becuase the key and value are the same in each. could be written add: add etc. 
      add,
      getAll,
      addListItem
   };
 })();
-
+// adding player to test add(player) - success
 playerListRepository.add({name: 'Justin Jefferson', salary: 8800, position: ['Offense', 'WR', 'FLX', 'SFLX' ]});
-playerListRepository.getAll().forEach(function(list) {
-    //without this call below, list does not display
-    playerListRepository.addListItem(list);
+
+playerListRepository.getAll().forEach(function(player) {
+    /*without this call below, list does not display. this forEach() is what runs through all the array objects and executes all of 
+    the addListItem() on each object of the array - gives them a button, adds event, etc */
+    playerListRepository.addListItem(player);
 });
-console.log(playerListRepository.getAll());
+//console.log(playerListRepository.getAll());
