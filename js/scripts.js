b645ecd3d38a71edf3b598ed2a2a80f76960fa31
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-07-10 16:08:34 -0400

    - going from players to pokemon
    - this commit changes the theme of the list from nfl players to pokemon. this is due to the availability of api

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,35 @@
-//IIFE employed. protects variables
-let playerListRepository = (function () {
-  let playerList = [
+/*IIFE employed. protects variables. to access pokemonList[], add(), getAll(), or addListItem(), pokemonListRepository
+must first be envoked - see the add() and getAll().forEach() calls below  . see js functions pt 2 exercise 1.5 of full stack emersion.*/
+let pokemonListRepository = (function () {
+  let pokemonList = [
     { name: 'Patrick Mahomes', salary: 9500, position: ['Offense', 'QB', 'SFLX']},
     { name: 'Josh Allen', salary: 9800, position: ['Offense', 'QB', 'SFLX']},
     { name: 'Jonathan Taylor', salary: 10000, position: ['Offense', 'RB', 'FLX', 'SFLX']},
     { name: 'New Orleans Saints', salary: 3500, position: ['Defense', 'DST']}
-];
+   ];
   
-  function add(player) {
-    //adds a new player to the list when executed. must pass name, salary, and positions as arguments
-        playerList.push(player);
+  function add(pokemon) {
+    //adds a new pokemon to the list when executed. must pass name, salary, and positions as arguments
+        pokemonList.push(pokemon);
     }
+
   function getAll() {
-    // returns the list of players and their details
-        return playerList;
+    // returns the list of pokemon and their details
+        return pokemonList;
     }
-    function addListItem(player) {
-        // .player-list is the class of the ul
-        let listElement = document.querySelector('.player-list');
+
+  function addListItem(pokemon) {
+        // .pokemon-list is the class of the ul
+        let listElement = document.querySelector('.pokemon-list');
         //console.log(listElement);
         // creates li for the ul
         let listItem = document.createElement('li');
         // creates a button and variable called button, and adds an event handler for the click event. handler prints name to log
         let button = document.createElement('button');
         button.addEventListener('click', function() {
-            showDetails(player);
+            showDetails(pokemon);
         });
-        button.innerText = player.name;
+        button.innerText = pokemon.name;
         console.log(button.innerText);
         //gives variable button a class so it can be uniquely manipulated in css
         button.classList.add('nameButton');
@@ -35,23 +38,27 @@ let playerListRepository = (function () {
         // attachs the li to the ul. created elements need to be appended. 
         listElement.appendChild(listItem);
     }
-    function showDetails(player) {
-        // this is the function called by the button click event on lines 25-26
-        console.log(player);
+
+    function showDetails(pokemon) {
+        // this is the function called by the button click event
+        console.log(pokemon);
     }
    return {
-    // the below code takes this form becuase the key and value are the same in each. could be written add: add etc. 
+    /* the return portion of the IIFE is essential as it is what allows you to access the listed functions elsewhere in the code.  
+    the below code takes this form becuase the key and value are the same in each. could be written add: add etc. */
      add,
      getAll,
      addListItem
   };
 })();
-// adding player to test add(player) - success
-playerListRepository.add({name: 'Justin Jefferson', salary: 8800, position: ['Offense', 'WR', 'FLX', 'SFLX' ]});
 
-playerListRepository.getAll().forEach(function(player) {
+// adding pokemon to test add(pokemon) - success
+pokemonListRepository.add({name: 'Justin Jefferson', salary: 8800, position: ['Offense', 'WR', 'FLX', 'SFLX' ]});
+
+pokemonListRepository.getAll().forEach(function(pokemon) {
     /*without this call below, list does not display. this forEach() is what runs through all the array objects and executes all of 
     the addListItem() on each object of the array - gives them a button, adds event, etc */
-    playerListRepository.addListItem(player);
+    pokemonListRepository.addListItem(pokemon);
 });
-//console.log(playerListRepository.getAll());
+
+//console.log(pokemonListRepository.getAll());
